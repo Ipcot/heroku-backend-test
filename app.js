d282628d390d5137f4bf279881f090282040ae26
Yuriy Pokhyl <yuriypokhyl@Yuriys-Air.Dlink>
2022-10-19 21:42:05 +0300

    add patch users/avatars

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const authRouter = require("./routes/api/auth");
 const contactsRouter = require("./routes/api/contacts");
 
+
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -13,6 +14,10 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
+
+
+
 
 app.use("/api/users", authRouter);
 app.use("/api/contacts", contactsRouter);
--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 
 const { User } = require("../../models/user");
 
@@ -13,11 +14,13 @@ const register = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
 
   const result = await User.create({
     email,
     password: hashPassword,
     subscription,
+    avatarURL,
   });
 
   res.status(201).json({
